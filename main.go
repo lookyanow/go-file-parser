33a86401c8770371ec5a0e520865addcb26b3e21
Lukyanov Ivan <ilukyanov@ozon.ru>
2019-02-05 15:27:03 +0300

    test parse file added with substring match

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+)
 
-func main(){
-	fmt.Println("New file parser")
+func main() {
+
+	file, err := os.Open("raw-test-file")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	str, err1 := getStringFromFile(file)
+	if err1 != nil {
+		panic(err1)
+	}
+	file.Close()
+	reg := regexp.MustCompile(`(?m)0;31m([^=.]*)\[0m`)
+	matches := reg.FindStringSubmatch(str)
+	fmt.Printf("%v", matches[1])
+
+}
+
+func getStringFromFile(r io.Reader) (string, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
